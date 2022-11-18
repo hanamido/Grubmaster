8836d59e64977a340334c7555fe083a38852e500
hanamilktea <dohan@oregonstate.edu>
2022-11-17 17:46:10 -0800

    updated files

--- grubmaster-handlebars/public/js/restaurantHasCuisines_ShowHideCRUD.js
+++ grubmaster-handlebars/public/js/restaurantHasCuisines_ShowHideCRUD.js
@@ -22,15 +22,16 @@ function showform(dowhat) {
     }
     else if (dowhat == 'browse') { //by default display browse
         document.getElementById('search-restaurant-cuisine').style.display = 'block'; 
-        document.getElementById('browse-restaurants-cuisines').style.display = 'block';
+        document.getElementById('browse-restaurant-cuisines').style.display = 'block';
         document.getElementById('add-restaurant-cuisine').style.display = 'none';
         document.getElementById('update-restaurant-cuisine').style.display = 'none';
     }
 }
+
+function browseRestaurantCuisines() { showform('browse'); }
 function searchRestaurantCuisine() {showform('search'); }
 function newRestaurantCuisine() { showform('insert'); }
 function updateRestaurantCuisine() { 
     showform('update'); 
 }; 
-function browseRestaurantCuisines() { showform('browse'); }
 function showAll() { showform('all'); }
