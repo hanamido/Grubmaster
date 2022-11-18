030c74d742201421c8db5ec7fa42287336058488
hanamilktea <dohan@oregonstate.edu>
2022-11-17 18:07:02 -0800

    updated search function

--- grubmaster-handlebars/app.js
+++ grubmaster-handlebars/app.js
@@ -463,7 +463,7 @@ app.get('/restaurant_has_cuisines', function(req, res) {   // Display all Restau
         INNER JOIN Cuisines on Restaurant_has_cuisines.cuisine_id = Cuisines.cuisine_id;`; 
     }
     // If there is a query string, we assume this is a search, and return desired results
-    if (req.query.rc_restaurantSearch !== undefined && req.query.rc_cuisineSearch === undefined) {
+    if (req.query.rc_restaurantSearch !== undefined) {
         showRestaurantCuisinesQuery = `SELECT restaurant_cuisine_id, Restaurants.restaurant_name as restaurants, Cuisines.cuisine_name as cuisines FROM Restaurant_has_cuisines 
         INNER JOIN Restaurants on Restaurant_has_cuisines.restaurant_id = Restaurants.restaurant_id 
         INNER JOIN Cuisines on Restaurant_has_cuisines.cuisine_id = Cuisines.cuisine_id
